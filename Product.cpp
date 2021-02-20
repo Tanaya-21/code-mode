5886a5bd79f9f2f7e4ab25dd832b1c0c6393df92
Tanaya-21 <64308988+Tanaya-21@users.noreply.github.com>
2021-02-20 18:58:31 +0530

    Create Product.cpp

---
+++ Product.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y , Product;
+	cout<<"Enter two numbers: "<<endl;
+	cin>>x>>y;
+	Product = x*y;
+	cout<<"Product = "<<Product<<endl;
+}
